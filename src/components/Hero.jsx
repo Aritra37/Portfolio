dff101f70c54a045db8cd7aa7d68903c713a1cea
Aritra37 <muhuriaritra37@gmail.com>
2023-07-30 04:45:02 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion';
 import {styles} from '../style';
-import {ComputersCanvas} from './canvas';
+import ComputersCanvas from './canvas/Computers';
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
@@ -25,6 +25,17 @@ const Hero = () => {
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-items-center items-start p-2'>
+            <motion.dev
+              animate={{
+                y:[0,24,0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary'
+            />
 
           </div>
         </a>
