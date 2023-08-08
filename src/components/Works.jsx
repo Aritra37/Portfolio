530397cc06e1cbc75e964d48b4959af707c09cfb
Aritra37 <muhuriaritra37@gmail.com>
2023-08-08 19:58:50 +0530

    included works and icons section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import Tilt from 'react-parallax-tilt';
+import {motion} from 'framer-motion';
+import {styles} from '../style';
+import {github} from '../assets';
+import {SectionWrapper} from '../hoc';
+import {projects} from '../constants';
+import { fadeIn,textVariant } from '../utils/motion';
 const Works = () => {
   return (
-    <div>Works</div>
-  )
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className='w-full flex'>
+        <motion.p variants={fadeIn(0,0,0.1,1)} className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+          Following projects showcase my skills and experience through. Each project is briefly described with links of code
+        </motion.p>
+      </div>
+    </>
+  );
 }
 
-export default Works
+export default SectionWrapper(Works,"");
